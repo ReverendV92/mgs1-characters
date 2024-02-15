cb898eacb719d2b7aa7e6273d3608367160efd6a
ReverendV92 <ReverendV92@users.noreply.github.com>
2024-02-15 02:16:13 -0600

    otacon & grey fox wip

---
+++ .content/.original/MGS_-_Gray_Fox_Playermodel_mgs_-_gray_fox_playermodel_2014-01-30-0057/lua/autorun/gray_fox_playermodel_cvp.lua
@@ -0,0 +1,3 @@
+player_manager.AddValidModel( "Gray Fox_cvp", 					"models/player/lordvipes/Metal_Gear_Rising/gray_fox_playermodel_cvp.mdl" )
+
+list.Set( "PlayerOptionsModel",  "Gray Fox_cvp", 					"models/player/lordvipes/Metal_Gear_Rising/gray_fox_playermodel_cvp.mdl" )
---
+++ .content/.original/Otacon_MGS1_3130451425_2024-01-03-1150/lua/autorun/sm.lua
@@ -0,0 +1,2 @@
+player_manager.AddValidModel( "ota", 				"models/ota/ota.mdl" )
+list.Set( "PlayerOptionsModel",  "ota",				"models/ota/ota.mdl" )
